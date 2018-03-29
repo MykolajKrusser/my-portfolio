f6c8223d2f65dc84b689e58b533b314432b1bbcc
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-03-29 16:35:15 +0200

    v1.3 fixed some styles for rwd

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -25,6 +25,25 @@ window.onload = function loadStart(){
     },1000);
 };
 /*
+Sticky nav bar
+ */
+// When the user scrolls the page, fire scrollSticky
+window.onscroll = function () {scrollSticky()};
+// Get the navigation bar from html
+var nav = document.getElementById("nav");
+// Get the offset position of the  navigation bar
+var sticky = nav.offsetTop;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollSticky() {
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky");
+        nav.firstElementChild.lastElementChild.classList.add("pulse");
+
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
+/*
 Start Quote Rotator
  */
 $( function() {
@@ -47,11 +66,6 @@ $( function() {
     $( '#cbp-qtrotator' ).cbpQTRotator();
 
 } );
-
-/* ---------------------------------------------- /*
- * E-mail validation
- /* ---------------------------------------------- */
-
 
 (function($){
     $(document).ready(function() {
